da3c35fc3a14ee2143ca30891eecf3aa4cab1212
Stas Bekman <stas@stason.org>
2018-10-19 16:58:38 -0700

    simplify/cleanup

--- toc.js
+++ toc.js
@@ -3,28 +3,14 @@
   $.fn.toc = function(options) {
     var defaults = {
       noBackToTopLinks: false,
-      title: '<i>Jump to...</i>',
+      title: '',
       minimumHeaders: 3,
-      headers: 'h1, h2, h3, h4, h5, h6',
+      headers: 'h1, h2, h3, h4',
       listType: 'ol', // values: [ol|ul]
       showEffect: 'show', // values: [show|slideDown|fadeIn|none]
-      showSpeed: 'slow', // set to 0 to deactivate effect
-      classes: { list: '',
-                 item: ''
-               }
+      showSpeed: 'slow' // set to 0 to deactivate effect
     },
     settings = $.extend(defaults, options);
-
-    function fixedEncodeURIComponent (str) {
-      return encodeURIComponent(str).replace(/[!'()*]/g, function(c) {
-        return '%' + c.charCodeAt(0).toString(16);
-      });
-    }
-
-    function createLink (header) {
-      var innerText = (header.textContent === undefined) ? header.innerText : header.textContent;
-      return "<a href='#" + fixedEncodeURIComponent(header.id) + "'>" + innerText + "</a>";
-    }
 
     var headers = $(settings.headers).filter(function() {
       // get all headers with an ID
@@ -35,7 +21,6 @@
       return this.id;
     }), output = $(this);
     if (!headers.length || headers.length < settings.minimumHeaders || !output.length) {
-      $(this).hide();
       return;
     }
 
@@ -50,13 +35,13 @@
       none: function() { output.html(html); }
     };
 
-    var get_level = function(ele) { return parseInt(ele.nodeName.replace("H", ""), 10); };
+    var get_level = function(ele) { return parseInt(ele.nodeName.replace("H", ""), 10); }
     var highest_level = headers.map(function(_, ele) { return get_level(ele); }).get().sort()[0];
     var return_to_top = '<i class="icon-arrow-up back-to-top"> </i>';
 
     var level = get_level(headers[0]),
       this_level,
-      html = settings.title + " <" +settings.listType + " class=\"" + settings.classes.list +"\">";
+      html = settings.title + " <"+settings.listType+">";
     headers.on('click', function() {
       if (!settings.noBackToTopLinks) {
         window.location.hash = this.id;
@@ -68,20 +53,21 @@
       if (!settings.noBackToTopLinks && this_level === highest_level) {
         $(header).addClass('top-level-header').after(return_to_top);
       }
+      txt = header.textContent.split('¶')[0].split('(')[0];
+      if (!txt) {return;}
       if (this_level === level) // same level as before; same indenting
-        html += "<li class=\"" + settings.classes.item + "\">" + createLink(header);
+        html += "<li><a href='#" + header.id + "'>" + txt + "</a>";
       else if (this_level <= level){ // higher level than before; end parent ol
-        for(var i = this_level; i < level; i++) {
+        for(i = this_level; i < level; i++) {
           html += "</li></"+settings.listType+">"
         }
-        html += "<li class=\"" + settings.classes.item + "\">" + createLink(header);
+        html += "<li><a href='#" + header.id + "'>" + txt + "</a>";
       }
       else if (this_level > level) { // lower level than before; expand the previous to contain a ol
         for(i = this_level; i > level; i--) {
-          html += "<" + settings.listType + " class=\"" + settings.classes.list +"\">" +
-                  "<li class=\"" + settings.classes.item + "\">"
+          html += "<"+settings.listType+">"+((i-level == 2) ? "<li class=\"hide_content\">" : "<li>")
         }
-        html += createLink(header);
+        html += "<a href='#" + header.id + "'>" + txt + "</a>";
       }
       level = this_level; // update for the next one
     });
